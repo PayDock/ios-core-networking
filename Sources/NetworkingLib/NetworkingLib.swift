d0a5a0b124c12e9b9f60a5e9e95940fb359f8b49
Domagoj Grizelj <domagoj.grizelj@gmail.com>
2024-06-19 23:45:48 +0200

    Initial commit

--- Sources/NetworkingLib/NetworkingLib.swift
+++ Sources/NetworkingLib/NetworkingLib.swift
@@ -1,2 +1,16 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  NetworkingLib.swift
+//  NetworkingLib
+//
+//  Copyright © 2024 Paydock Ltd.
+//  Created by Domagoj Grizelj on 14.06.2024..
+//
+
+public class NetworkingLib {
+
+    public static let shared = NetworkingLib()
+
+    var publicKeyHash: String = ""
+    var host: String = ""
+
+}
